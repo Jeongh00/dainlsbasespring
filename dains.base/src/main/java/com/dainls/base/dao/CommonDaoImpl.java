edcd003552765fcf8bbfaa9b7ab122b45d1c3852
unknown <rustic606@gmail.com>
2022-03-15 11:22:30 +0900

    .gitignore

---
+++ dains.base/src/main/java/com/dainls/base/dao/CommonDaoImpl.java
@@ -0,0 +1,365 @@
+package com.dainls.base.dao;
+
+import java.util.HashMap;
+import java.util.List;
+
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+import com.dainls.base.dto.SearchDTO;
+import com.dainls.base.global.BaseDao;
+import com.dainls.base.global.SysKeyword;
+
+import org.apache.ibatis.session.SqlSession;
+
+
+/**
+ * @author test
+ *
+ * @FILENAME : CommonDaoImpl.java
+ * @PROJECT : dains.base
+ * @DATE : 2022. 3. 11.
+ * @USER : test
+ * @CHANGELOG : 오후 3:50:24
+ * @SUBJECT : 
+ */
+
+
+@Repository("commonDao") @Primary
+public class CommonDaoImpl extends BaseDao implements ICommonDao{
+	
+	protected String result = "";
+	public static final int COMMON_UPDATE_NUMBER_DAO = SysKeyword.COMMON_UPDATE_NUMBER;
+	
+	public CommonDaoImpl(SqlSession sqlSession) {
+		super(sqlSession);
+	}
+
+	/**
+	 * @param paramMap
+	 * @param menuKeyword
+	 * @param DaoKeyword
+	 * @return List<?>
+	 * @FILENAME : CommonDaoImpl.java
+	 * @PROJECT : dains.base
+	 * @DATE : 2022. 3. 11.
+	 * @USER : test
+	 * @CHANGELOG : 오후 3:54:32
+	 * @METHOD : 
+	 * 
+	 */
+	@Override        
+	public List<?> list(HashMap<String, Object> paramMap, String menuKeyword, String DaoKeyword) {		
+		result = getLocation(menuKeyword, DaoKeyword);
+		return selectList(result, paramMap);
+	}
+
+	/**
+	 * @param paramMap
+	 * @param menuKeyword
+	 * @param DaoKeyword
+	 * @return List<?>
+	 * @FILENAME : CommonDaoImpl.java
+	 * @PROJECT : dains.base
+	 * @DATE : 2022. 3. 11.
+	 * @USER : test
+	 * @CHANGELOG : 오후 3:54:52
+	 * @METHOD : 
+	 * 
+	 */
+	@Override
+	public List<?> list(SearchDTO paramMap, String menuKeyword, String DaoKeyword) {
+		result = getLocation(menuKeyword, DaoKeyword);
+		return selectList(result, paramMap);
+	}
+	
+	@Override
+	public int totalCount(HashMap<String, Object> paramMap, String menuKeyword, String DaoKeyword) {
+		result = getLocation(menuKeyword, DaoKeyword);
+		return (int) selectOne(result, paramMap);
+	}
+
+	
+	@Override
+	public int totalCount(SearchDTO paramMap, String menuKeyword, String DaoKeyword) {
+		result = getLocation(menuKeyword, DaoKeyword);
+		return (int) selectOne(result, paramMap);
+	}
+
+	/**
+	 * @param paramMap
+	 * @param menuKeyword
+	 * @param DaoKeyword
+	 * @return HashMap<String, Object>
+	 * @FILENAME : CommonDaoImpl.java
+	 * @PROJECT : dains.base
+	 * @DATE : 2022. 3. 11.
+	 * @USER : test
+	 * @CHANGELOG : 오후 3:55:03
+	 * @METHOD : 
+	 * 
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public HashMap<String, Object> detail(HashMap<String, Object> paramMap, String menuKeyword, String DaoKeyword) {
+		result = getLocation(menuKeyword, DaoKeyword);
+		return (HashMap<String, Object>) selectOne(result, paramMap);
+	}
+
+	/**
+	 * @param paramMap
+	 * @param menuKeyword
+	 * @param DaoKeyword
+	 * @return  HashMap<String, Object>
+	 * @FILENAME : CommonDaoImpl.java
+	 * @PROJECT : dains.base
+	 * @DATE : 2022. 3. 11.
+	 * @USER : test
+	 * @CHANGELOG : 오후 3:55:00
+	 * @METHOD : 
+	 * 
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public HashMap<String, Object> detail(SearchDTO paramMap, String menuKeyword, String DaoKeyword) {
+		result = getLocation(menuKeyword, DaoKeyword);
+		return (HashMap<String, Object>) selectOne(result, paramMap);
+	}
+
+	/**
+	 * @param paramMap
+	 * @param menuKeyword
+	 * @param DaoKeyword
+	 * @return 0~99
+	 * @FILENAME : CommonDaoImpl.java
+	 * @PROJECT : dains.base
+	 * @DATE : 2022. 3. 11.
+	 * @USER : test
+	 * @CHANGELOG : 오후 3:55:09
+	 * @METHOD : 
+	 * 
+	 */
+	@Override
+	public int create(HashMap<String, Object> paramMap, String menuKeyword, String DaoKeyword) {
+		result = getLocation(menuKeyword, DaoKeyword);
+		return update(result, paramMap);
+	}
+
+	/**
+	 * @param paramMap
+	 * @param menuKeyword
+	 * @param DaoKeyword
+	 * @return 0~99
+	 * @FILENAME : CommonDaoImpl.java
+	 * @PROJECT : dains.base
+	 * @DATE : 2022. 3. 11.
+	 * @USER : test
+	 * @CHANGELOG : 오후 3:55:12
+	 * @METHOD : 
+	 * 
+	 */
+	@Override
+	public int create(SearchDTO paramMap, String menuKeyword, String DaoKeyword) {
+		result = getLocation(menuKeyword, DaoKeyword);
+		return update(result, paramMap);
+	}
+
+	/**
+	 * @param paramMap
+	 * @param menuKeyword
+	 * @param DaoKeyword
+	 * @return true, false
+	 * @FILENAME : CommonDaoImpl.java
+	 * @PROJECT : dains.base
+	 * @DATE : 2022. 3. 11.
+	 * @USER : test
+	 * @CHANGELOG : 오후 3:55:16
+	 * @METHOD : 
+	 * 
+	 */
+	@Override
+	public boolean isCreate(HashMap<String, Object> paramMap, String menuKeyword, String DaoKeyword) {
+		result = getLocation(menuKeyword, DaoKeyword);
+		int _result = update(result, paramMap); 
+		boolean result = (_result ==  COMMON_UPDATE_NUMBER_DAO) ? true : false;
+		return result;
+	}
+
+	/**
+	 * @param paramMap
+	 * @param menuKeyword
+	 * @param DaoKeyword
+	 * @return true, false
+	 * @FILENAME : CommonDaoImpl.java
+	 * @PROJECT : dains.base
+	 * @DATE : 2022. 3. 11.
+	 * @USER : test
+	 * @CHANGELOG : 오후 3:57:11
+	 * @METHOD : 
+	 * 
+	 */
+	@Override
+	public boolean isCreate(SearchDTO paramMap, String menuKeyword, String DaoKeyword) {
+		result = getLocation(menuKeyword, DaoKeyword);
+		int _result = update(result, paramMap); 
+		boolean result = (_result ==  COMMON_UPDATE_NUMBER_DAO) ? true : false;
+		return result;
+	}
+
+	/**
+	 * @param paramMap
+	 * @param menuKeyword
+	 * @param DaoKeyword
+	 * @FILENAME : CommonDaoImpl.java
+	 * @PROJECT : dains.base
+	 * @DATE : 2022. 3. 11.
+	 * @USER : test
+	 * @CHANGELOG : 오후 3:57:34
+	 * @METHOD : 
+	 * 
+	 */
+	@Override
+	public void voidCreate(HashMap<String, Object> paramMap, String menuKeyword, String DaoKeyword) {
+		result = getLocation(menuKeyword, DaoKeyword);
+		update(result, paramMap); 
+	}
+
+	/**
+	 * @param paramMap
+	 * @param menuKeyword
+	 * @param DaoKeyword
+	 * @FILENAME : CommonDaoImpl.java
+	 * @PROJECT : dains.base
+	 * @DATE : 2022. 3. 11.
+	 * @USER : test
+	 * @CHANGELOG : 오후 3:57:49
+	 * @METHOD : 
+	 * 
+	 */
+	@Override
+	public void voidCreate(SearchDTO paramMap, String menuKeyword, String DaoKeyword) {
+		result = getLocation(menuKeyword, DaoKeyword);
+		update(result, paramMap); 
+	}
+
+	/**
+	 * @param paramMap
+	 * @param menuKeyword
+	 * @param DaoKeyword
+	 * @return 0~999
+	 * @FILENAME : CommonDaoImpl.java
+	 * @PROJECT : dains.base
+	 * @DATE : 2022. 3. 11.
+	 * @USER : test
+	 * @CHANGELOG : 오후 3:58:08
+	 * @METHOD : 
+	 * 
+	 */
+	@Override
+	public int update(HashMap<String, Object> paramMap, String menuKeyword, String DaoKeyword) {
+		result = getLocation(menuKeyword, DaoKeyword);
+		return update(result, paramMap);
+	}
+
+	/**
+	 * @param paramMap
+	 * @param menuKeyword
+	 * @param DaoKeyword
+	 * @return 0~999
+	 * @FILENAME : CommonDaoImpl.java
+	 * @PROJECT : dains.base
+	 * @DATE : 2022. 3. 11.
+	 * @USER : test
+	 * @CHANGELOG : 오후 3:58:12
+	 * @METHOD : 
+	 * 
+	 */
+	@Override
+	public int update(SearchDTO paramMap, String menuKeyword, String DaoKeyword) {
+		result = getLocation(menuKeyword, DaoKeyword);
+		return update(result, paramMap);
+	}
+
+	/**
+	 * @param paramMap
+	 * @param menuKeyword
+	 * @param DaoKeyword
+	 * @return  true, false
+	 * @FILENAME : CommonDaoImpl.java
+	 * @PROJECT : dains.base
+	 * @DATE : 2022. 3. 11.
+	 * @USER : test
+	 * @CHANGELOG : 오후 3:58:16
+	 * @METHOD : 
+	 * 
+	 */
+	@Override
+	public boolean isUpdate(HashMap<String, Object> paramMap, String menuKeyword, String DaoKeyword) {
+		result = getLocation(menuKeyword, DaoKeyword);
+		int _result = update(result, paramMap); 
+		boolean result = (_result ==  COMMON_UPDATE_NUMBER_DAO) ? true : false;
+		return result;
+	}
+
+	/**
+	 * @param paramMap
+	 * @param menuKeyword
+	 * @param DaoKeyword
+	 * @return  true, false
+	 * @FILENAME : CommonDaoImpl.java
+	 * @PROJECT : dains.base
+	 * @DATE : 2022. 3. 11.
+	 * @USER : test
+	 * @CHANGELOG : 오후 3:58:19
+	 * @METHOD : 
+	 * 
+	 */
+	@Override
+	public boolean isUpdate(SearchDTO paramMap, String menuKeyword, String DaoKeyword) {
+		result = getLocation(menuKeyword, DaoKeyword);
+		int _result = update(result, paramMap); 
+		boolean result = (_result == COMMON_UPDATE_NUMBER_DAO) ? true : false;
+		return result;
+	}
+
+	/**
+	 * @param paramMap
+	 * @param menuKeyword
+	 * @param DaoKeyword
+	 * @FILENAME : CommonDaoImpl.java
+	 * @PROJECT : dains.base
+	 * @DATE : 2022. 3. 11.
+	 * @USER : test
+	 * @CHANGELOG : 오후 3:59:13
+	 * @METHOD : 
+	 * 
+	 */
+	@Override
+	public void voidUpdate(HashMap<String, Object> paramMap, String menuKeyword, String DaoKeyword) {
+		result = getLocation(menuKeyword, DaoKeyword);
+		update(result, paramMap);
+		
+	}
+
+	/**
+	 * @param paramMap
+	 * @param menuKeyword
+	 * @param DaoKeyword
+	 * @FILENAME : CommonDaoImpl.java
+	 * @PROJECT : dains.base
+	 * @DATE : 2022. 3. 11.
+	 * @USER : test
+	 * @CHANGELOG : 오후 3:59:17
+	 * @METHOD : 
+	 * 
+	 */
+	@Override
+	public void voidUpdate(SearchDTO paramMap, String menuKeyword, String DaoKeyword) {
+		result = getLocation(menuKeyword, DaoKeyword);
+		update(result, paramMap);
+		
+	}
+
+}
+	
+
---
+++ dains.base/src/main/java/com/dainls/base/global/BaseDao.java
@@ -0,0 +1,72 @@
+package com.dainls.base.global;
+
+import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import egovframework.rte.psl.dataaccess.EgovAbstractDAO;
+
+public class BaseDao extends EgovAbstractDAO {
+	
+	protected void printQueryId(String queryId) {
+		if (logger.isDebugEnabled()) {
+			logger.debug("\t QueryId  \t:  " + queryId);
+			logger.info("\t QueryId  \t:  " + queryId);
+		}else if(logger.isWarnEnabled()){
+			logger.warn("\t QueryId  \t:  " + queryId);
+		}else if(logger.isErrorEnabled()){
+			logger.error("\t QueryId_error  \t:  " + queryId);
+		}else{
+			logger.info("\t QueryId  \t:  " + queryId);
+		}
+	}
+	
+	protected String getLocation(String menuKeyword, String DaoKeyword) {
+		String _result = menuKeyword + SysKeyword.DOT + DaoKeyword;
+		printQueryId(_result);
+		return _result;
+	}
+	
+	private static final Logger logger = LoggerFactory.getLogger(BaseDao.class);
+	
+	private SqlSession sqlSession;
+	
+	@Autowired
+	public BaseDao(SqlSession sqlSession) {
+		this.sqlSession = sqlSession;
+	}
+	
+	public Object insert(String queryId, Object params) {
+		return sqlSession.insert(queryId, params);
+	}
+	
+
+	public int update(String queryId, Object params) {
+		return sqlSession.update(queryId, params);
+	}
+
+	public int delete(String queryId, Object params) {
+		return sqlSession.delete(queryId, params);
+	}
+
+	public Object selectOne(String queryId) {
+		return sqlSession.selectOne(queryId);
+	}
+
+	public Object selectOne(String queryId, Object params) {
+		return sqlSession.selectOne(queryId, params);
+	}
+
+	@SuppressWarnings("rawtypes")
+	public List selectList(String queryId) {
+		return sqlSession.selectList(queryId);
+	}
+
+	@SuppressWarnings("rawtypes")
+	public List selectList(String queryId, Object params) {
+		return sqlSession.selectList(queryId, params);
+	}
+	
+}
